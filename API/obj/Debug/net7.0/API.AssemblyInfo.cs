e5c794ef0bda3c47b37b13668cb6412acaa2be20
George Michael <michaege@sheridancollege.ca>
2024-04-12 06:00:12 -0400

    api commit

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f6100b4efffc7126ca173e243e0ca54cd36587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770c14aee1d92a211b498275c1948056004a4cfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
