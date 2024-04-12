770c14aee1d92a211b498275c1948056004a4cfa
George Michael <michaege@sheridancollege.ca>
2024-04-12 05:04:43 -0400

    api data

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0abef04db01a7a5aac068db0fbd19de8ff6f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f6100b4efffc7126ca173e243e0ca54cd36587")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
